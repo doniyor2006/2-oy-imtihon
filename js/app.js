27aa27bbc52bd235774e81b994bf10458dab070a
Doniyor <example@gmail.com>
2022-12-30 21:09:12 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,105 @@
+var elSectiondroptop = document.querySelector(".section__drop-top");
+var elsectioninformation = document.querySelector(".section__information");
+var elsectionicon = document.querySelector(".section__img");
+elSectiondroptop.addEventListener("click", function(){
+ elsectioninformation.classList.toggle("show-text");
+ elsectionicon.classList.toggle("turn")
+ elsectioninformation2.classList.remove("show-text");
+ elsectionicon2.classList.remove("turn")
+ elsectioninformation3.classList.remove("show-text");
+ elsectionicon3.classList.remove("turn")
+ elsectioninformation4.classList.remove("show-text");
+ elsectionicon4.classList.remove("turn")
+ elsectioninformation5.classList.remove("show-text");
+ elsectionicon5.classList.remove("turn")
+ elsectioninformation6.classList.remove("show-text");
+ elsectionicon6.classList.remove("turn")
+});
+
+var elSectiondroptop2= document.querySelector(".section__drop-top2");
+var elsectioninformation2 = document.querySelector(".section__information2");
+var elsectionicon2 = document.querySelector(".section__img2");
+elSectiondroptop2.addEventListener("click", function(){
+ elsectioninformation2.classList.toggle("show-text");
+ elsectionicon2.classList.toggle("turn")
+ elsectioninformation.classList.remove("show-text");
+ elsectionicon.classList.remove("turn")
+ elsectioninformation3.classList.remove("show-text");
+ elsectionicon3.classList.remove("turn")
+ elsectioninformation4.classList.remove("show-text");
+ elsectionicon4.classList.remove("turn")
+ elsectioninformation5.classList.remove("show-text");
+ elsectionicon5.classList.remove("turn")
+ elsectioninformation6.classList.remove("show-text");
+ elsectionicon6.classList.remove("turn")
+});
+
+var elSectiondroptop3= document.querySelector(".section__drop-top3");
+var elsectioninformation3 = document.querySelector(".section__information3");
+var elsectionicon3 = document.querySelector(".section__img3");
+elSectiondroptop3.addEventListener("click", function(){
+ elsectioninformation3.classList.toggle("show-text");
+ elsectionicon3.classList.toggle("turn")
+ elsectioninformation.classList.remove("show-text");
+ elsectionicon.classList.remove("turn")
+ elsectioninformation2.classList.remove("show-text");
+ elsectionicon2.classList.remove("turn")
+ elsectioninformation4.classList.remove("show-text");
+ elsectionicon4.classList.remove("turn")
+ elsectioninformation5.classList.remove("show-text");
+ elsectionicon5.classList.remove("turn")
+ elsectioninformation6.classList.remove("show-text");
+ elsectionicon6.classList.remove("turn")
+});
+var elSectiondroptop4= document.querySelector(".section__drop-top4");
+var elsectioninformation4 = document.querySelector(".section__information4");
+var elsectionicon4 = document.querySelector(".section__img4");
+elSectiondroptop4.addEventListener("click", function(){
+ elsectioninformation4.classList.toggle("show-text");
+ elsectionicon4.classList.toggle("turn")
+ elsectioninformation.classList.remove("show-text");
+ elsectionicon.classList.remove("turn")
+ elsectioninformation2.classList.remove("show-text");
+ elsectionicon2.classList.remove("turn")
+ elsectioninformation3.classList.remove("show-text");
+ elsectionicon3.classList.remove("turn")
+ elsectioninformation5.classList.remove("show-text");
+ elsectionicon5.classList.remove("turn")
+ elsectioninformation6.classList.remove("show-text");
+ elsectionicon6.classList.remove("turn")
+});
+var elSectiondroptop5= document.querySelector(".section__drop-top5");
+var elsectioninformation5 = document.querySelector(".section__information5");
+var elsectionicon5 = document.querySelector(".section__img5");
+elSectiondroptop5.addEventListener("click", function(){
+    elsectioninformation5.classList.toggle("show-text");
+ elsectionicon5.classList.toggle("turn")
+ elsectioninformation.classList.remove("show-text");
+ elsectionicon.classList.remove("turn")
+ elsectioninformation2.classList.remove("show-text");
+ elsectionicon2.classList.remove("turn")
+ elsectioninformation3.classList.remove("show-text");
+ elsectionicon3.classList.remove("turn")
+ elsectioninformation4.classList.remove("show-text");
+ elsectionicon4.classList.remove("turn")
+ elsectioninformation6.classList.remove("show-text");
+ elsectionicon6.classList.remove("turn")
+ 
+});
+var elSectiondroptop6= document.querySelector(".section__drop-top6");
+var elsectioninformation6 = document.querySelector(".section__information6");
+var elsectionicon6 = document.querySelector(".section__img6");
+elSectiondroptop6.addEventListener("click", function(){
+    elsectioninformation6.classList.toggle("show-text");
+ elsectionicon6.classList.toggle("turn")
+ elsectioninformation.classList.remove("show-text");
+ elsectionicon.classList.remove("turn")
+ elsectioninformation2.classList.remove("show-text");
+ elsectionicon2.classList.remove("turn")
+ elsectioninformation3.classList.remove("show-text");
+ elsectionicon3.classList.remove("turn")
+ elsectioninformation4.classList.remove("show-text");
+ elsectionicon4.classList.remove("turn")
+ elsectioninformation5.classList.remove("show-text");
+ elsectionicon5.classList.remove("turn")
+});
